4339f991326da460275ec36eb5c66ae71ed00a6e
Retky <jose.arredondo.a@outlook.com>
2022-07-05 17:27:07 -0500

    Modify 'models' to improve foreign keys

--- db/migrate/20220705212727_create_likes.rb
+++ db/migrate/20220705212727_create_likes.rb
@@ -2,10 +2,9 @@ class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
       t.references :author_id, null: false
-      t.references :post, null: false, foreign_key: true
+      t.references :author, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end
-    add_foreign_key :likes, :users, column: :author_id
   end
 end
