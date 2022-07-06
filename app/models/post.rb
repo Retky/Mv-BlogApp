5810bd283be1c04f340cfdcd70d59382ce93e961
Retky <jose.arredondo.a@outlook.com>
2022-07-06 16:41:35 -0500

    Add 'Post' methods
    to update user & recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,15 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+  after_save :update_user_posts_counter
+
+  private
+
+  def update_user_posts_counter
+    user.increment!(:posts_counter)
+  end
 end
