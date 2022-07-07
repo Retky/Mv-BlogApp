fb7394c836701ed78740176f8656f5dcbce46bb9
Retky <jose.arredondo.a@outlook.com>
2022-07-07 17:22:31 -0500

    Add 'comment' specs

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  user = User.create(name: 'Rick', photo: 'https://th.bing.com/', bio: 'Some guy')
+  post = Post.create(title: 'Some title', text: 'Some text', author_id: user.id)
+  subject { Comment.new(text: 'Some text', post_id: post.id, author_id: user.id) }
+
+  before { subject.save }
+
+  it 'belongs to an author' do
+    expect(subject.author_id).to eq(user.id)
+  end
+
+  it 'belongs to a post' do
+    expect(subject.post_id).to eq(post.id)
+  end
+end
