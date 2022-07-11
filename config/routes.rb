4094a123e5275bdb7c9e3b147cff2f49f3328605
Retky <jose.arredondo.a@outlook.com>
2022-07-11 11:12:02 -0500

    Add 'Posts & Single Post' routes with controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "users#index"
-  get "/users" => "users#index", as: :users
-  get "/users/:id" => "users#show", as: :user
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
