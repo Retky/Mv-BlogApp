9f4fa1ee6d42e40dbe434ca9b1d5ac9f2524bd4b
Retky <jose.arredondo.a@outlook.com>
2022-07-14 17:38:36 -0500

    Add 'new Post' route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,30 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @user = current_user
+    if @post.save
+      puts 'Post created successfully'
+      redirect_to user_posts_path(current_user)
+    else
+      render :new, alert: 'Error occured!'
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text, :author_id)
   end
 end
